a1b179f6fc87714c1b20de68617a41335b82e3a7
lake429c <have2nthusiasm@gmail.com>
2020-04-05 14:28:01 +0900

    迷路を生成し終えてから，ボタンにクリックイベントをつけるように修正

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 'use strict';
 
+// min以上max未満の乱数を得る関数
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//// コモン ////
+
 // 迷路の一辺のサイズ
-const size = 100;
+const size = 40;
 
 /*
   迷路のマスの状態
@@ -9,19 +18,13 @@ const size = 100;
   1:通路（白）
   2:スタート（赤）
   3:ゴール（緑）
+  4:最短路（青）
 */
 let maze = [];
 // 壁で埋める
 for(let i=0;i<size;i++){
   let tmp = [];
   for(let j=0;j<size;j++){
-    /*
-    if(i == 0 || i == size-1 || j == 0 || j == size-1){
-      tmp.push(1)
-    }else{
-      tmp.push(0);
-    }
-    */
     tmp.push(0);
   }
   maze.push(tmp);
@@ -32,23 +35,11 @@ function drawMase(){
   const canvas = document.getElementById('maze_canvas');
   const ctx = canvas.getContext("2d");
 
+  // 一マスの一辺のサイズ
   const rect_size = 10;
+  // キャンバスの大きさの決定
   canvas.width = (size+1)*rect_size;
   canvas.height = (size+1)*rect_size;
-
-  // 格子の描画
-  /*
-  ctx.lineWidth = 0.5;
-  ctx.strokeStyle = "rgb(200,200,200)";
-  ctx.beginPath();
-  for(let i=0;i<size+1;i++){
-    ctx.moveTo(i*rect_size+1, 0);
-    ctx.lineTo(i*rect_size+1, size*rect_size+2);
-    ctx.moveTo(0, i*rect_size+1);
-    ctx.lineTo(size*rect_size+2, i*rect_size+1);
-  }
-  ctx.stroke();
-  */
 
   for(let i=0;i<size;i++){
     for(let j=0;j<size;j++){
@@ -66,170 +57,116 @@ function drawMase(){
         case 3:
           ctx.fillStyle = "rgb(0,255,0)";
           break;
-        default:
-          ctx.fillStyle = "rgb(0,0,255)";
+        case 4:
+          ctx.fillStyle = "rgb(0,100,255)";
           break;
       }
       // マスの描画
-      ctx.fillRect(j*rect_size, i*rect_size, rect_size, rect_size);
+      ctx.fillRect(j*rect_size, i*rect_size, rect_size-1, rect_size-1);
     }
   }
-
 }
 
 drawMase();
 
-// 乱数を得る関数
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+//// ジェネレータ ////
+
+// (x,y)のdirection方向のマスが掘れる
+// 0:左 1:右 2:上 3:下
+function isDigable(x, y, direction){
+  switch (direction) {
+    case 0:
+      if(y-1 != 0
+        && maze[x][y-1] == 0
+        && maze[x][y-2] == 0
+        && maze[x-1][y-1] == 0
+        && maze[x+1][y-1] == 0
+      ) return true;
+      break;
+    case 1:
+      if(y+1 != size-1
+        && maze[x][y+1] == 0
+        && maze[x][y+2] == 0
+        && maze[x-1][y+1] == 0
+        && maze[x+1][y+1] == 0
+      ) return true;
+      break;
+    case 2:
+      if(x-1 != 0
+        && maze[x-1][y] == 0
+        && maze[x-2][y] == 0
+        && maze[x-1][y-1] == 0
+        && maze[x-1][y+1] == 0
+      ) return true;
+      break;
+    case 3:
+      if(x+1 != size-1
+        && maze[x+1][y] == 0
+        && maze[x+2][y] == 0
+        && maze[x+1][y-1] == 0
+        && maze[x+1][y+1] == 0
+      ) return true;
+      break;
+  }
+  return false;
 }
 
-// ランダムな奇数を得る関数
-function getRandomOdd(min, max){
-  return getRandomInt(0, (max-1)/2) * 2 + 1;
-}
-
-// どの方向にも進めない
-function isNotDigable(x, y){
-  let flg = true;
-  /*
-  if(maze[x][y-1] == 0 && maze[x][y-2] == 0) flg = false;
-  if(maze[x][y+1] == 0 && maze[x][y+2] == 0) flg = false;
-  if(maze[x-1][y] == 0 && maze[x-2][y] == 0) flg = false;
-  if(maze[x+1][y] == 0 && maze[x+2][y] == 0) flg = false;
-  */
-  if(maze[x][y-1] == 0 && y-1 != 0 && maze[x][y-2] == 0 && maze[x-1][y-1] == 0 && maze[x+1][y-1] == 0) flg = false;
-  if(maze[x][y+1] == 0 && y+1 != size-1 && maze[x][y+2] == 0 && maze[x-1][y+1] == 0 && maze[x+1][y+1] == 0) flg = false;
-  if(maze[x-1][y] == 0 && x-1 != 0 && maze[x-2][y] == 0 && maze[x-1][y-1] == 0 && maze[x-1][y+1] == 0) flg = false;
-  if(maze[x+1][y] == 0 && x+1 != size-1 && maze[x+2][y] == 0 && maze[x+1][y-1] == 0 && maze[x+1][y+1] == 0) flg = false;
-  return flg;
+// (x,y)周りにまだ掘れるマスがある
+function isDigableAround(x, y){
+  for(let i=0;i<4;i++){
+    if(isDigable(x, y, i)) return true;
+  }
+  return false;
 }
 
 // 一歩進む
 function goForward(x, y){
-  let direct;
-  let flg = true;
+
+  let direction = getRandomInt(0, 4);
+
   // 行く方向を確認して決める
-  while(flg){
-    direct = getRandomInt(0, 4);
-    /*
-    switch (direct) {
-      case 0:
-        if(maze[x][y-1] == 0 && maze[x][y-2] == 0) flg = false;
-        break;
-      case 1:
-        if(maze[x][y+1] == 0 && maze[x][y+2] == 0) flg = false;
-        break;
-      case 2:
-        if(maze[x-1][y] == 0 && maze[x-2][y] == 0) flg = false;
-        break;
-      case 3:
-        if(maze[x+1][y] == 0 && maze[x+2][y] == 0) flg = false;
-        break;
-      default:
-        break;
-    }
-    */
-    switch (direct) {
-      case 0:
-        if(maze[x][y-1] == 0 && y-1 != 0 && maze[x][y-2] == 0 && maze[x-1][y-1] == 0 && maze[x+1][y-1] == 0) flg = false;
-        break;
-      case 1:
-        if(maze[x][y+1] == 0 && y+1 != size-1 && maze[x][y+2] == 0 && maze[x-1][y+1] == 0 && maze[x+1][y+1] == 0) flg = false;
-        break;
-      case 2:
-        if(maze[x-1][y] == 0 && x-1 != 0 && maze[x-2][y] == 0 && maze[x-1][y-1] == 0 && maze[x-1][y+1] == 0) flg = false;
-        break;
-      case 3:
-        if(maze[x+1][y] == 0 && x+1 != size-1 && maze[x+2][y] == 0 && maze[x+1][y-1] == 0 && maze[x+1][y+1] == 0) flg = false;
-        break;
-      default:
-        break;
-    }
+  while(!isDigable(x, y, direction)){
+    direction = getRandomInt(0, 4);
   }
   // 決めた方向に進む
-  switch (direct) {
-    //上
+  switch (direction) {
     case 0:
       maze[x][y-1] = 1;
-      //maze[x][y-2] = 1;
-      //y -= 2;
       y--;
       break;
-    //下
     case 1:
       maze[x][y+1] = 1;
-      //maze[x][y+2] = 1;
-      //y += 2;
       y++;
       break;
-    //左
     case 2:
       maze[x-1][y] = 1;
-      //maze[x-2][y] = 1;
-      //x -= 2;
       x--;
       break;
-    //右
     case 3:
       maze[x+1][y] = 1;
-      //maze[x+2][y] = 1;
-      //x += 2;
       x++;
-      break;
-    default:
       break;
   }
   drawMase();
-  if(!isNotDigable(x,y)) setTimeout(goForward, 0, x, y);
-  else setTimeout(jenerateMaze, 0);
+  //if(isDigableAround(x,y)) setTimeout(goForward, 0, x, y);
+  //else setTimeout(jenerateMaze, 0);
+  if(isDigableAround(x,y)) goForward(x, y);
+  else jenerateMaze();
 }
 
-// 掘り終わった判定関数
+// 掘り終わった判定
+// どの通路マスも周りが掘れないなら終了
 function isDone(){
   let cnt = 0;
-  /*
-  for(let i=0;i<(size-1)/2;i++){
-    for(let j=0;j<(size-1)/2;j++){
-      if(maze[i*2+1][j*2+1] == 0) return false;
-    }
-  }
-  */
   for(let i=1;i<size-1;i++){
     for(let j=1;j<size-1;j++){
-      if(maze[i][j] != 0 && !isNotDigable(i,j)) {
+      if(maze[i][j] != 0 && isDigableAround(i,j)) {
         return false;
       }
     }
   }
   return true;
 }
-
-// 最初に一マス穴開ける
-//maze[getRandomOdd(0,size)][getRandomOdd(0,size)] = 1;
-maze[getRandomInt(1,size-1)][getRandomInt(1,size-1)] = 1;
-
-let que = [];
-let checked = [];
-for(let i=0;i<size;i++){
-  let tmp = [];
-  for(let j=0;j<size;j++){
-    tmp.push(false);
-  }
-  checked.push(tmp);
-}
-let dist = [];
-for(let i=0;i<size;i++){
-  let tmp = [];
-  for(let j=0;j<size;j++){
-    tmp.push(size*size);
-  }
-  dist.push(tmp);
-}
-let start = [];
-let goal = [];
 
 // 行き止まり判定
 function isDeadEnd(x, y){
@@ -245,133 +182,128 @@ function isDeadEnd(x, y){
 // 穴掘り法
 function jenerateMaze(){
 
-  let flg = true;
-  let x, y;
-  while(flg && !isDone()){
-    // 掘り始める通路マスを決める
-    //x = getRandomOdd(0,size);
-    //y = getRandomOdd(0,size);
-    x = getRandomInt(1,size-1);
-    y = getRandomInt(1,size-1);
-    if(!isNotDigable(x,y) && maze[x][y] == 1) flg = false;
-  }
-  console.log(x);
-  console.log(y);
+  let x = getRandomInt(1, size-1), y = getRandomInt(1, size-1);
 
   if(!isDone()){
+    // 掘り始める通路マスを決める
+    while(!(maze[x][y] == 1 && isDigableAround(x,y))){
+      x = getRandomInt(1,size-1);
+      y = getRandomInt(1,size-1);
+    }
     goForward(x, y);
   }else{
-    //もう掘るとこなくなったら，外周を壁にする
-    for(let i=0;i<size;i++){
-      for(let j=0;j<size;j++){
-        if(i == 0 || i == size-1 || j == 0 || j == size-1){
-          maze[i][j] = 0;
-        }
-      }
+    //スタートの設定
+    while(!(maze[x][y] == 1 && isDeadEnd(x, y))){
+      x = getRandomInt(1,size);
+      y = getRandomInt(1,size);
     }
-    //スタートとゴールの設定
-    let flg = true;
-    while(flg){
-      let x = getRandomInt(1,size);
-      let y = getRandomInt(1,size);
-      if(maze[x][y] == 1 && isDeadEnd(x, y)){
-        maze[x][y] = 2;
-        start.push(x);
-        start.push(y);
-        que.push([x,y]);
-        dist[x][y] = 0;
-        flg = false;
-      }
+    maze[x][y] = 2;
+    // ゴールの設定
+    while(!(maze[x][y] == 1 && isDeadEnd(x, y))){
+      x = getRandomInt(1,size);
+      y = getRandomInt(1,size);
     }
-    flg = true;
-    while(flg){
-      let x = getRandomInt(1,size);
-      let y = getRandomInt(1,size);
-      if(maze[x][y] == 1 && isDeadEnd(x, y)){
-        maze[x][y] = 3;
-        goal.push(x);
-        goal.push(y);
-        flg = false;
-      }
-    }
+    maze[x][y] = 3;
+
     drawMase();
-    console.log("Done");
+    console.log("Jenerated");
+    // ボタンにクリックイベントを紐づけ
+    let solveEle = document.getElementById('solveBt');
+    solveEle.onclick = onSolveClick;
   }
 }
 
+// 最初に一マス穴開ける
+maze[getRandomInt(1,size-1)][getRandomInt(1,size-1)] = 1;
 jenerateMaze();
 
+//// ソルバー ////
+
+// 幅優先探索のキュー
+let que = [];
+// 探索済みフラグ
+let checked = [];
+for(let i=0;i<size;i++){
+  let tmp = [];
+  for(let j=0;j<size;j++){
+    tmp.push(false);
+  }
+  checked.push(tmp);
+}
+// スタートからの距離
+let dist = [];
+for(let i=0;i<size;i++){
+  let tmp = [];
+  for(let j=0;j<size;j++){
+    tmp.push(size*size);
+  }
+  dist.push(tmp);
+}
+
+// 幅優先探索で迷路を解く
 function solveMaze(){
+
+  let x, y;
   let cnt = 0;
-  while(que[cnt][0] != goal[0] || que[cnt][1] != goal[1]){
-    let x = que[cnt][0], y = que[cnt][1];
-    let min = dist[x][y];
+
+  // スタート地点を探す
+  for(let i=1;i<size-1;i++){
+    for(let j=1;j<size-1;j++){
+      if(maze[i][j] == 2){
+        x = i;
+        y = j;
+        que.push([x,y]);
+        dist[x][y] = 0;
+      }
+    }
+  }
+  // ゴールを探して進む
+  while(maze[x][y] != 3){
     if(maze[x-1][y] != 0 && checked[x-1][y] == false){
       que.push([x-1,y]);
-    }else if(maze[x-1][y] != 0 && checked[x-1][y] == true){
-      if(dist[x-1][y] < min) min = dist[x-1][y];
+    }else if(checked[x-1][y] == true){
+      dist[x][y] = dist[x-1][y]+1;
     }
     if(maze[x+1][y] != 0 && checked[x+1][y] == false){
       que.push([x+1,y]);
-    }else if(maze[x+1][y] != 0 && checked[x+1][y] == true){
-      if(dist[x+1][y] < min) min = dist[x+1][y];
+    }else if(checked[x+1][y] == true){
+      dist[x][y] = dist[x+1][y]+1;
     }
     if(maze[x][y-1] != 0 && checked[x][y-1] == false){
       que.push([x,y-1]);
-    }else if(maze[x][y-1] != 0 && checked[x][y-1] == true){
-      if(dist[x][y-1] < min) min = dist[x][y-1];
+    }else if(checked[x][y-1] == true){
+      dist[x][y] = dist[x][y-1]+1;
     }
     if(maze[x][y+1] != 0 && checked[x][y+1] == false){
       que.push([x,y+1]);
-    }else if(maze[x][y+1] != 0 && checked[x][y+1] == true){
-      if(dist[x][y+1] < min) min = dist[x][y+1];
+    }else if(checked[x][y+1] == true){
+      dist[x][y] = dist[x][y+1]+1;
     }
-    dist[x][y] = min+1;
     checked[x][y] = true;
-    console.log(que[cnt] + "," + dist[x][y]);
     cnt++;
+    x = que[cnt][0], y = que[cnt][1];
+  }
+  // ゴールから最短路をたどる
+  while(maze[x][y] != 2){
+    if(maze[x-1][y] != 0 && dist[x-1][y] < dist[x][y]){
+      x--;
+    }
+    else if(maze[x+1][y] != 0 && dist[x+1][y] < dist[x][y]){
+      x++;
+    }
+    else if(maze[x][y-1] != 0 && dist[x][y-1] < dist[x][y]){
+      y--;
+    }
+    else if(maze[x][y+1] != 0 && dist[x][y+1] < dist[x][y]){
+      y++;
+    }
+    if(maze[x][y] != 2) maze[x][y] = 4;
   }
   drawMase();
-  // ゴールから最短路をたどる
-  let flg = true;
-  let x = goal[0], y = goal[1];
-  while(flg){
-    let min = size*size;
-    let tmp_x, tmp_y;
-    if(maze[x-1][y] != 0 && dist[x-1][y] < min){
-      min = dist[x-1][y];
-      tmp_x = x-1;
-      tmp_y = y;
-    }
-    if(maze[x+1][y] != 0 && dist[x+1][y] < min){
-      min = dist[x+1][y];
-      tmp_x = x+1;
-      tmp_y = y;
-    }
-    if(maze[x][y-1] != 0 && dist[x][y-1] < min){
-      min = dist[x][y-1];
-      tmp_x = x;
-      tmp_y = y-1;
-    }
-    if(maze[x][y+1] != 0 && dist[x][y+1] < min){
-      min = dist[x][y+1];
-      tmp_x = x;
-      tmp_y = y+1;
-    }
-    x = tmp_x;
-    y = tmp_y;
-    if(x == start[0] && y == start[1]){
-      flg = false;
-    }else{
-      maze[x][y] = 4;
-    }
-  }
+  console.log("Solved!");
 }
 
 // solveボタンの実装
 function onSolveClick(){
-  let solveEle = document.getElementById('solve');
   solveMaze();
-  drawMase();
-  console.log("Solved!");
 }
