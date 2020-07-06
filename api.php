666d68d6088a325c73fdec132b9de19056deebf8
lake429c <have2nthusiasm@gmail.com>
2020-07-06 22:00:32 +0900

    phpのテスト

--- api.php
+++ api.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head></head>
-  <body>
-    <?php
-    echo <<<EOM
-    <script type="text/javascript" src="poker.js" charset="utf-8"></script>
-    EOM;
-    ?>
-  </body>
-</html>
+<?php
+  $test = "hoge";
+  print($test);
+?>
